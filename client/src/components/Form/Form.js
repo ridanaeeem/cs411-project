e402b2edb99e43b01a32d30d8a15b7f6d1b52cc9
ridanaeeem <ridan@bu.edu>
2023-12-05 22:54:36 -0500

    added some
    error checking to form

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -3,7 +3,7 @@ import FileBase from "react-file-base64";
 import { useDispatch } from "react-redux";
 import { createRecipe } from "../../actions/recipes";
 
-export function Form(){
+export function Form() {
 	const [recipeData, setRecipeData] = useState({
 		title: "",
 		description: "",
@@ -16,57 +16,107 @@ export function Form(){
 	// so we can dispatch the actions
 	const dispatch = useDispatch();
 
+	// when the user clicks the submit button, validate the data before dispatching
+	function validate(recipeData) {
+		// map recipedata
+		for (var prop in recipeData) {
+			// go through each property in the recipeData object
+			if (Object.prototype.hasOwnProperty.call(recipeData, prop)) {
+				if (recipeData[prop].includes("<") || recipeData[prop].includes(">")) {
+					alert(`Please enter a valid ${prop}, no special characters (<, >, {, }) allowed`);
+					return false;
+				} else if (recipeData[prop].includes("{") || recipeData[prop].includes("}")) {
+					alert(`Please enter a valid ${prop}, no special characters (<, >, {, }) allowed`);
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	// when the user submits the form, dispatch the action to create the recipe
 	const handleSubmit = (e) => {
-		console.log("submit");
 		// send over post request with the data entered by user
 		// so the form doesn't clear when the user submits, in case error checking needs to happen
 		e.preventDefault();
 		// dispatch action to create the recipe
 		console.log("Recipe Data:", recipeData);
-		dispatch(createRecipe(recipeData));
+		if (validate(recipeData)) {
+			dispatch(createRecipe(recipeData));
+		} else {
+			console.log("Error");
+		}
 	};
 
-	const clear = () => {
-		console.log("clear");
+	// when the user clicks the clear button, clear the form
+	const handleClear = (e) => {
+		e.preventDefault();
+		setRecipeData({
+			title: "",
+			description: "",
+			ingredients: "",
+			instructions: "",
+			tags: "",
+			recipePhoto: "",
+		});
 	};
 
 	return (
 		<>
-			<form onSubmit={handleSubmit}>
+			<form id="recipeForm" onSubmit={handleSubmit}>
 				<label for="titleInput">Title </label>
 				<br></br>
 				<input
 					id="titleInput"
+					type="text"
 					value={recipeData.title}
-					onChange={(e) => setRecipeData({ ...recipeData, title: e.target.value })}></input>
+					onChange={(e) => setRecipeData({ ...recipeData, title: e.target.value })}
+					required
+					maxlength="50"
+					form="recipeForm"
+				/>
 				<br></br>
 				<label for="descriptionInput">Description </label>
 				<br></br>
 				<input
 					id="descriptionInput"
 					value={recipeData.description}
-					onChange={(e) => setRecipeData({ ...recipeData, description: e.target.value })}></input>
+					onChange={(e) => setRecipeData({ ...recipeData, description: e.target.value })}
+					required
+					maxlength="300"
+					form="recipeForm"
+				/>
 				<br></br>
 				<label for="ingredientsInput">Ingredients </label>
 				<br></br>
 				<input
 					id="ingredientsInput"
 					value={recipeData.ingredients}
-					onChange={(e) => setRecipeData({ ...recipeData, ingredients: e.target.value })}></input>
+					onChange={(e) => setRecipeData({ ...recipeData, ingredients: e.target.value })}
+					required
+					maxlength="50"
+					form="recipeForm"
+				/>
 				<br></br>
 				<label for="instructionsInput">Instructions </label>
 				<br></br>
 				<input
 					id="instructionsInput"
 					value={recipeData.instructions}
-					onChange={(e) => setRecipeData({ ...recipeData, instructions: e.target.value })}></input>
+					onChange={(e) => setRecipeData({ ...recipeData, instructions: e.target.value })}
+					required
+					maxlength="200"
+					form="recipeForm"
+				/>
 				<br></br>
 				<label for="tagsInput">Tags </label>
 				<br></br>
 				<input
 					id="tagsInput"
 					value={recipeData.tags}
-					onChange={(e) => setRecipeData({ ...recipeData, tags: e.target.value })}></input>
+					onChange={(e) => setRecipeData({ ...recipeData, tags: e.target.value })}
+					maxlength="20"
+				/>
 				<br></br>
 				{/* photo upload currently does not work - table functionality for now  */}
 				{/* <label for="photoInput">Photo Upload</label>
@@ -78,11 +128,15 @@ export function Form(){
 						multiple={false}
 						onDone={(base64) => setRecipeData({ ...recipeData, recipePhoto: base64 })}></FileBase>
 				</div> */}
-				<button onClick={handleSubmit}>Submit</button>
-				<button onClick={clear}>Clear</button>
+				<button type="submit" id="submit">
+					Submit
+				</button>
+				<button type="button" onClick={handleClear}>
+					Clear
+				</button>
 			</form>
 		</>
 	);
-};
+}
 
 export default Form;
--- client/src/components/Recipes/Recipe/Recipe.js
+++ client/src/components/Recipes/Recipe/Recipe.js
@@ -1,18 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+
+const RecipeCard = styled.div`
+	border: 1px solid black;
+	margin: 10px;
+	padding: 10px;
+`;
 
 export function Recipe({ recipe }) {
-	// only want to display some of the description on the recipe cards 
+	// only want to display some of the description on the recipe cards
 	let shortDescription = recipe.description;
 	if (shortDescription.length > 10) {
 		shortDescription = recipe.description.substring(0, 10) + "...";
 	}
 
+	console.log(recipe.creator);
+
 	return (
-		<>
+		<RecipeCard>
 			<h1>{recipe.title}</h1>
-			<h3>{recipe.description}</h3>
+			<p>{recipe.creator}</p>
 			<p>{shortDescription}</p>
-		</>
+		</RecipeCard>
 	);
 }
 
