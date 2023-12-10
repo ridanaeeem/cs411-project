6467fe21a40f6124f0ac6f11b9c24744322bba5f
ridanaeeem <ridan@bu.edu>
2023-12-10 00:00:37 -0500

    shows your current recipes now

---
+++ client/src/components/Recipes/YourRecipeCards.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { useSelector } from "react-redux"; //allows us to fetch data from global redux store
+
+import RecipeCard from "./Recipe/RecipeCard";
+import { Link } from "react-router-dom";
+
+export function YourRecipeCards({ setCurRecipe }) {
+	const user = JSON.parse(localStorage.getItem("profile"));
+	const email = user?.email;
+	const recipes = useSelector((state) => state.recipes);
+
+	const recipeItems = recipes.map((recipe) => {
+		const atIndex = recipe.creator.indexOf("@");
+		const username = recipe.creator.slice(0, atIndex);
+		const title = recipe.title.replace(/\s+/g, "-").toLowerCase();
+		return recipe.creator === email ? (
+			<Link key={recipe._id} to={`/${username}/${title}`} onClick={() => setCurRecipe(recipe)}>
+				<RecipeCard recipe={recipe} />
+			</Link>
+		) : (
+			<></>
+		);
+	});
+
+	return !recipes.length ? (
+		<h1>No recipes yet!</h1>
+	) : (
+		<>
+			<h1>Recipes</h1>
+			<div>{recipeItems}</div>
+		</>
+	);
+}
+
+export default YourRecipeCards;
--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,21 +1,24 @@
 import React, { useEffect } from "react";
-import Recipes from "../Recipes/RecipeCards";
-import Form from "../Form/Form";
+import YourRecipeCards from "../Recipes/YourRecipeCards";
 import { useDispatch } from "react-redux";
 import { getRecipes } from "../../actions/recipes";
 import { Login } from "../Login";
 
 export function Profile() {
-	// dispatch an action (getRecipes) to the redux store
+	// dispatch an action to the redux store
 	const dispatch = useDispatch();
 
 	useEffect(() => {
 		dispatch(getRecipes());
 	}, [dispatch]);
 
+	const user = JSON.parse(localStorage.getItem("profile"));
+	const email = user?.email;
+
 	return (
 		<>
-			<h1>welcome to your main profile page</h1>
+			<h1>welcome to your online cookbook {user?.username}!</h1>
+			<YourRecipeCards></YourRecipeCards>
 			<div></div>
 		</>
 	);
