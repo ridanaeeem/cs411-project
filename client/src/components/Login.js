f2d8c0b7200431237d80e8c9c9b6db1ad0b89251
ridanaeeem <ridan@bu.edu>
2023-12-09 20:43:06 -0500

    popup box logic

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -8,6 +8,8 @@ import { useNavigate } from "react-router-dom";
 
 const LogInDiv = styled.div``;
 
+const PopUpBox = styled.div``;
+
 export function Login() {
 	// cool google sign in prompt but it doesnt work all the time
 	/* global google */
@@ -15,12 +17,26 @@ export function Login() {
 
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
+
+	const [decodedCredentialResponse, setDecodedCredentialResponse] = useState(null);
+	const [showPopup, setShowPopup] = useState(false);
+	const [username, setUsername] = useState("");
+
 	// when a user signs in with google, a json web token is returned
 	// this token contains encrypted information about the user that needs to be decrypted using jwt-decode
 	const onSuccessRef = (credentialResponse) => {
 		console.log(credentialResponse);
-		const decodedCredentialResponse = jwtDecode(credentialResponse.credential);
-		console.log(decodedCredentialResponse);
+		const decodedResponse = jwtDecode(credentialResponse.credential);
+		console.log(decodedResponse);
+		setDecodedCredentialResponse(decodedResponse);
+		setShowPopup(true);
+	};
+
+	const onErrorRef = () => {
+		console.log("error");
+	};
+
+	const submitSignUp = (decodedCredentialResponse) => {
 		try {
 			// dispatch the decodedCredentialResponse to the redux store
 			dispatch({ type: "LOGIN", data: { decodedCredentialResponse } });
@@ -31,13 +47,19 @@ export function Login() {
 		}
 	};
 
-	const onErrorRef = () => {
-		console.log("error");
-	};
-
 	return (
 		<LogInDiv>
 			<GoogleLogin onSuccess={onSuccessRef} onError={onErrorRef} cookiePolicy="single_host_origin" />
+			{showPopup ? (
+				<PopUpBox>
+					Welcome new user!<br></br>
+					What would you like your username to be?
+					<form>
+						<input type="text" onChange={(e) => setUsername(e.target.value)} />
+					</form>
+					<button onClick={() => submitSignUp(decodedCredentialResponse)}>Submit</button>
+				</PopUpBox>
+			) : null}
 		</LogInDiv>
 	);
 }
---
+++ server/models/user.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+// create schema
+const userSchema = mongoose.Schema({
+	username: {
+		type: String,
+		default: "",
+		required: true,
+	},
+	email: {
+		type: String,
+		default: "",
+		required: true,
+	},
+	likes: {
+		type: [String],
+		default: undefined,
+	},
+});
+
+const Use = mongoose.model("Users", userSchema);
+
+// export mongoose model to perform CRUD operations on
+export default User;
