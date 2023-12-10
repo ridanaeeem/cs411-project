645d7987ceb76ef1082b450a555dcdd8e3d6011c
ridanaeeem <ridan@bu.edu>
2023-12-09 23:27:38 -0500

    storing email and username now

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -24,7 +24,7 @@ export function Login() {
 		const username = email.slice(0, atIndex);
 		try {
 			// dispatch the user info to the redux store
-			dispatch({ type: "LOGIN", data: { username } });
+			dispatch({ type: "LOGIN", data: { email, username } });
 			// redirect to dashboard after login
 			navigate("/recipes");
 		} catch (error) {
